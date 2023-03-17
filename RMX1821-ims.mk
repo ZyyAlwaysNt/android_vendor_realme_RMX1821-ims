2094fd90654143f15aedde01c9d1eed3f5e1aed5
Arrowsploit <arrowsploit@pm.me>
2023-03-17 04:49:29 +0000

    RMX1821: Initial commit
    * IMS Service & Bootjars from: full_oppo6771-user 10 QP1A.190711.020 08805472cb0b87b3 release-keys
    
    Co-authored-by: Matsvei Niaverau's avatarMatsvei Niaverau <matvej2002@gmail.com>
    Co-authored-by: Sahil Sonar's avatarSahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: default avatarArrowsploit <arrowsploit@pm.me>

---
+++ RMX1821-ims.mk
@@ -0,0 +1,18 @@
+PRODUCT_PACKAGES += \
+    ImsService \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
+
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
